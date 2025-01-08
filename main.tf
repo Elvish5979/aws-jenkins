c9f586712062c7dacbf07073b81fc3ae5fcd55ee
Elvish5979 <49043644+Elvish5979@users.noreply.github.com>
2025-01-08 14:27:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,19 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "t1"
-
-  instance_type          = "t2.micro"
-  key_name               = "Vishal-AWS-KEY"
-  monitoring             = false
-  vpc_security_group_ids = ["sg-0ca1c372cec02bf24"]
-  subnet_id              = "subnet-03408a59f77a6e364"
-
+resource "aws_instance" "example" {
+  ami           = "ami-01816d07b1128cd2d"
+  instance_type = "t2.micro"
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    Name = "ExampleInstance"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = ""
+    key    = "terraform.tfstate"
+    region = "us-east-1"
   }
 }
