f45cb896982a76a3184cdbc49b3c15fc3807a944
vishal <elvishranmale@gmail.com>
2025-08-22 10:58:01 +0530

    update

--- terraform/Dev/main.tf
+++ terraform/Dev/main.tf
@@ -1,11 +1,10 @@
 
 module "secure_bucket" {
   source             = "../../modules/S3-Bucket"
-  bucket_name        = "my-tf-example-bucket-bella"
+  bucket_name        = "my-tf-example-bucket-jenkins"
   versioning_enabled = true
   tags = {
     Environment = "dev"
     Owner       = "Vishal"
-    Project     = "CI/CD"
   }
 }
